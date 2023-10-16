3a40785d78808032e36a7e5191df9972bee37c7e
Denis Lebenkov <sabzz1ro3@mail.ru>
2023-10-16 18:49:18 +0300

    database was added

---
+++ Warehouse/Storage/Database.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Controls;
+
+namespace Warehouse
+{
+    internal class Database
+    {
+        static string connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=warehouse;Integrated Security=True;Connect Timeout=30;Encrypt=False";
+        SqlConnection sqlConnection = new SqlConnection(connection);
+
+        public void Open()
+        {
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+            else if (sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void Select(string query, DataGrid dataGrid)
+        {
+            Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGrid.ItemsSource = dataTable.DefaultView;
+            Open();
+        }
+    }
+}
